36fa27b3ff805531aef33c7ff90c92ce785b3b3c
HP <HP@LAPTOP-06RA4R2G>
2022-07-06 17:16:25 +0530

    First commit

---
+++ src/pack/TestClass5.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class TestClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Another Test Class");
+	}
+
+}
